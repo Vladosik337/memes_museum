ab59276f2a58e098eb893fbe90d703b641acbe21
Vladosik337 <vlad.ostapenko.337@gmail.com>
2025-08-13 01:04:16 +0300

    feat(styles): add exhibition-specific styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./markdown.css";
 import { NextAuthProvider } from "./providers/SessionProvider";
-
 
 export const metadata: Metadata = {
   title: "Create Next App",
