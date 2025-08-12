4d622b2c99691848cbad46c91ecddbeea004bb72
Vladosik337 <vlad.ostapenko.337@gmail.com>
2025-08-13 00:58:41 +0300

    feat(layout): create AdminLayout component

---
+++ app/admin/layout.tsx
@@ -0,0 +1,17 @@
+import AdminHeader from "@/components/admin/AdminHeader";
+import React from "react";
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <AdminHeader />
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        {children}
+      </main>
+    </div>
+  );
+}
