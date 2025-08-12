bc142e248658f820948e55f1a206f7b3d9a0195b
Vladosik337 <vlad.ostapenko.337@gmail.com>
2025-08-13 01:14:45 +0300

    feat(Header): refactor header layout for improved structure and readability

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,22 +6,36 @@ import Link from "next/link";
 export default function Header() {
   const { data: session } = useSession();
   const isAuth = !!session?.user;
+  const isAdmin = session?.user?.role === "admin";
   return (
     <header className="z-40 w-full bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-6">
-          <div className="flex items-center space-x-3">
-            <h1 className="logo text-gray-900 text-2xl font-bold">MuseMeme</h1>
-          </div>
+          <Link
+                href="/"
+                className="logo text-gray-900 hover:text-orange-600 transition-colors text-2xl font-bold"
+              >
+                MuseMeme
+              </Link>
           <div className="text-center text-gray-700 font-medium hidden md:block">
             Музей мемів всеосяжного Інтернету
           </div>
-          <Link
-            href={isAuth ? "/profile" : "/login"}
-            className="text-gray-900 underline hover:text-orange-600"
-          >
-            Перевірити квиток
-          </Link>
+          <div className="flex items-center space-x-4">
+            <Link
+              href={isAuth ? "/profile" : "/login"}
+              className="text-gray-900 underline hover:text-orange-600"
+            >
+              Перевірити квиток
+            </Link>
+            {isAdmin && (
+              <Link
+                href="/admin/exhibitions"
+                className="bg-orange-600 hover:bg-orange-700 text-white px-4 py-2 rounded-lg font-semibold"
+              >
+                Адмін-панель
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </header>
